3ba5e5c2dc87e921b1621ae7c7290776ac9c30d5
Felipe Rodrigues Costa <79774048+felipecosta2000@users.noreply.github.com>
2024-07-25 14:20:26 -0300

    Banco.js

--- DataBase/Banco.js
+++ DataBase/Banco.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/myDatabase', 
+mongoose.connect('mongodb://localhost:27017/banco-de dados', 
               {useNewUrlParser:true});
         
 mongoose.connection.once('open', ()=>{ 
